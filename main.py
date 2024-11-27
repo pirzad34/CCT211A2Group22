9940dc6c21e11baea26b8d84ec2a5462c54964c0
pirzad34 <syedhassan.pirzada@mail.utoronto.ca>
2024-11-27 11:51:15 -0500

    soccer stats concept

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+import tkinter
+import csv
+
+def read_data():
+    table = []
+    with open('soccer_stats.csv', mode ='r') as file:
+        csvFile = csv.reader(file)
+        for line in csvFile:
+            print(line)
+            table.append(line)
+    print(table)
+read_data()
